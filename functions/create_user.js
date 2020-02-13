fc2d442569a12fac3682043ce82852d353350fde
lucas-bresson <contact.bresson@gmail.com>
2020-02-13 20:37:25 +0100

    set up createUser to safely create user provided a phone

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,21 @@
+const admin = require('firebase-admin');
+
 module.exports = (req, res) => {
-  res.send(req.body);
+  // Verify the user provided a phone
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad input' });
+  }
+
+  // Format the phone number by replace any non-digits by ''
+  // make sure phone is a string as replace cannot be called w/ a number
+  const phone = String(req.body.phone).replace(/[^\d]/g, '');
+
+  // Create a new user account using that phone number
+  admin
+    .auth()
+    .createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
+
+  // Respond to the user request, saying the account was made
 };
